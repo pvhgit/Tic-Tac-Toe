f5e2275d754f2dbe4c49b8588bf9725e9797766e
pvh <pvhedaoo@gmail.com>
2015-01-21 13:53:12 -0500

    integrated front-end code

--- phTicServer.js
+++ phTicServer.js
@@ -7,9 +7,25 @@ var express = require("express");
 var app = express();
 var port = 3700;
  
-app.get("/", function(req, res){
-    res.send("Hello world!");
-});
+// ---------------- Web server -----------------------
  
-app.listen(port);
+app.set('views', __dirname + '/public');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("tictactoe");
+});
+
+app.use(express.static( __dirname + '/public') );
+
+var io = require('socket.io').listen(app.listen(port));
+
+// Socket connection handler from front-end
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: "Welcome to Prashant Hedaoo's TicTacToe" });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
 console.log("Listening on port " + port);
