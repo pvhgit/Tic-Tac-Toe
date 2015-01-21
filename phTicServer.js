68aeb00026d83f4646b457ddceb228f841c6c7ee
pvh <pvhedaoo@gmail.com>
2015-01-21 17:04:54 -0500

    Working Tic Tac Toe game with simple game engine

--- phTicServer.js
+++ phTicServer.js
@@ -1,4 +1,4 @@
-// ---- Tic tac toe gane for CMG ------
+// ---- Tic tac toe game for CMG ------
 // ---- Author: Prashant Hedaoo
 // ---- Server code conforming to REST principles
 // ---- Server does not keep track of previous states
@@ -22,10 +22,88 @@ var io = require('socket.io').listen(app.listen(port));
 
 // Socket connection handler from front-end
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: "Welcome to Prashant Hedaoo's TicTacToe" });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
+	// message handler for 'board_data' from front-end
+    socket.on('board_data', function (data) {
+        console.log("From client: ", data);
+        var move = game_processor(data.game);
+        socket.emit( move.evt, move.data );
     });
 });
 
 console.log("Listening on port " + port);
+
+// set game engine
+var game_plugin;
+
+try {
+	game_plugin = require('./non_losing_tictactoe');
+	console.log("Loaded Non losing TicTacToe game engine.")
+}
+catch (err) { // Can't load module; use default tic tac toe
+	console.log("Can't load Non losing TicTacToe game engine. Using simple tictactoe engine.")
+	game_plugin = simple_tictactoe_engine;
+
+}
+
+// ========================================================
+// ------------------------ game framework ----------------
+
+function game_processor(board) {
+	// Check if player won
+	if (isWin(board, 'X')) {
+		console.log("Player wins!!!");
+		return {  evt : 'game_over', data : { msg : 'You have won!!!', 'move' : board } };
+	}
+	// get next AI move
+	var move = nextMove(board);
+	// Check if AI won
+	if (isWin(move, 'O')) {
+		console.log("AI wins!!!", move);
+		return {  evt : 'game_over', data : { msg : 'You have lost!!!', 'move' : move } };
+	}
+	// Check if we have a tie
+	if (isTie(move)) {
+		console.log("TIE!!!");
+		return {  evt : 'game_over', data : { msg : 'Game is a tie!', 'move' : move } };
+	}
+	return {  evt : 'next_move', data : { 'move' : move } };
+}
+
+function isWin(board_data, mark) {
+	var board = [];
+    var tmp1 = board_data.split(',');
+    for (var n=0; n < tmp1.length-1; ++n)
+        board[n] = (tmp1[n].split(':')[1] === mark) ? 1 : 0;
+
+    if (    // Rows
+    	   (board[0] + board[1] + board[2] === 3)
+    	|| (board[3] + board[4] + board[5] === 3)
+    	|| (board[6] + board[7] + board[8] === 3)
+    		// Columns
+    	|| (board[0] + board[3] + board[6] === 3)
+    	|| (board[1] + board[4] + board[7] === 3)
+    	|| (board[2] + board[5] + board[8] === 3)
+    		// Diagonals
+    	|| (board[0] + board[4] + board[8] === 3)
+    	|| (board[2] + board[4] + board[6] === 3) )
+    	return true;
+    return false;
+}
+
+function isTie(board_data) {
+    return (board_data.indexOf(":E") === -1);
+}
+
+function nextMove(user_board) {
+	var game = game_plugin(user_board);
+	console.log("AI response:           " + game);
+	return game;
+}
+
+//--------------------- game engine code below ---------
+
+// simple_tictactoe_engine: Simply selects first empty cell as next move for AI
+function simple_tictactoe_engine(game_board) {
+	return game_board.replace(":E,", ":O,");
+}
+
--- public/tictactoeFE.js
+++ public/tictactoeFE.js
@@ -3,24 +3,149 @@
 // ---- client code
 
 
+var positions = [ '0_0', '0_1', '0_2',
+                  '1_0', '1_1', '1_2',
+                  '2_0', '2_1', '2_2'  ];
+var board;
+var socket;
+var blinker_id;
+
+// debug vars
+var debug = false;  // Set true for message log on page
+var messages = [];
+
+// main code
 window.onload = function() {
- 
-    var messages = [];
-    var socket = io.connect('http://localhost:3700');
- 
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data.message);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
-            }
-            $("#content").html(html);
-        } else {
-            console.log("There is a problem:", data);
+    
+    // connect to server
+    socket = io.connect('http://localhost:3700');
+
+    // Set 'next_move' message handler
+    socket.on('next_move', function (data) {
+        if(data && data.move) {
+            logg( null, "from server: " + data.move);
+            renderBoard(data.move);
         }
     });
 
-    // test msg out
-    socket.emit('send', { 'message' : 'Hi there! msg from Front end' } );
+    // Set 'game_over' message handler
+    socket.on('game_over', function (data) {
+        if(data) {
+            if (data.move) {
+                logg( null, "from server: " + data.move);
+                renderBoard(data.move);
+            }
+            logg( null, data.msg );
+            $("#status").css('color','yellow').css('background-color','black')
+                .removeClass('blink_text')
+                .html('<br>' + data.msg);
+            clearInterval(blinker_id); // stop blinking text
+        }
+        disableBoard();
+    });
+
+    // Start the game and initialize the page
+    renderBoard(null);
+    blinker_id = setInterval(blinker, 500); // start blinking text loop
+    $("#status").css('color','blue').addClass('blink_text')
+        .html("Welcome!<br> Let's play. Your turn...");
+ }
+
+function renderBoard(board_data) {
+    board = {};
+    if (board_data)
+        deSerializeBoard(board_data);
+    else // set all cells as empty
+        for( var n=0; n < positions.length; ++n)
+            board[positions[n]] = 'E';
+
+    drawCells();
+    $("#status").css('color','black').html("<br>Your turn....");
+
+    if (!debug)
+       $("#content").hide(); // debug activity messages
+};
+
+// render board based on values from server
+function drawCells() {
+    $("#game_layout").empty();
+    for(var i=0; i<positions.length;++i) {
+        var label = positions[i], div = '<div/>', cls = "cell " + label, clr = 'black';
+        if (board[label] === 'E') // empty cells
+            cls += " on"; // mark empty cells
+        else
+            div = "<div>" + board[label] + "</div>";
+        if (board[label] === 'O') clr = 'blue';
+        var cell = $(div).addClass(cls).appendTo("#game_layout").css('color',clr);
+        if ( i % 3 === 0 ) // insert row separarors
+            cell.before('<div class="clear"></div>');
+    }
+
+    // activate clicks and hover for all empty cells
+    $("#game_layout .on").bind('mouseover', hoverCell).bind('mouseout', leaveCell)
+        .bind("click", processClick);
+}
+
+function disableBoard() {
+    $("#game_layout .cell").unbind("click").unbind("mouseover").unbind("mouseout");
+};
+
+function processClick(ev) {
+    // mark clicked cell with X
+    $(this).trigger("mouseout").unbind("click mouseover mouseout").text('X');
+    board[$(this).attr('class').split(' ')[1]] = 'X';
+    logg($(this), 'processClick');
+
+    // send board data to server ###################################
+    socket.emit('board_data', { game : serializeBoard() });
+
+    $("#status").css('color','blue').html("<br>Thinking.....  wait");
+    disableBoard(); // disable board till next move from server
+    return false;
+};
+
+function hoverCell(ev) {
+    $(this).addClass("hover");
+    return false;
+};
+
+function leaveCell(ev) {
+    $(this).removeClass("hover");
+    return false;
+};
+
+function serializeBoard() {
+    var ot = '';
+    for(var b=0; b<positions.length; ++b)
+        ot += positions[b] + ":" + board[positions[b]] + ",";
+    return ot;
+}
+
+function deSerializeBoard(data) {
+    // Deserialize board data
+    var tmp1 = data.split(',');
+    for (var n=0; n < tmp1.length-1; ++n) {
+        var tmp2 = tmp1[n].split(':');
+        board[tmp2[0]] = tmp2[1];
+    }
+}
+
+function logg(msg, str) {
+    if (!debug)
+        return;
+
+    if (msg)
+        str += ": " + msg.attr('class');
+    messages.push(str);
+
+    str = '';
+    for(var i=0; i<messages.length; i++)
+        str += messages[i] + '<br/>';
+    $("#content").html(str);
+}
+
+// text blinker
+function blinker() {
+    $('.blink_text').fadeOut(500);
+    $('.blink_text').fadeIn(500);
 }
